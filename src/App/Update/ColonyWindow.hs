08ccc32a88a23d4201fe06703a21956fa5e74771
Viktor Tanyi <tanyi.viktor@gmail.com>
2018-09-09 20:10:39 +0200

    Refactor colony window

--- src/App/Update/ColonyWindow.hs
+++ src/App/Update/ColonyWindow.hs
@@ -15,74 +15,111 @@ import App.Model.GameState (GameState(..))
 import App.Model.BodyMinerals (MineralData(..))
 import App.Model.ShipBuildingTask (ShipBuildingTask(..))
 import App.Rect (Rect(..))
+import App.Uid (Uid)
 import App.Update.Updating (Updating)
+import App.Util (whenAlt)
 import Data.String (fromString)
+
+data Action
+  = BuildMine
+  | BuildShip
+  | FoundColony
 
 update :: GameState -> Updating GameState
 update gs = do
   Widget.window (Rect (V2 32 32) (V2 (4 + 200 + 4 + 256 + 4) (500 + 24))) 20 "Colonies"
   let p = V2 36 56
 
+  selectedBody <- bodyList p gs
+  gs' <- for selectedBody $ \Body{ uid } -> do
+    mineralTableHeight <- mineralTable (p + V2 (200 + 4) 0) uid gs
+    let q = p + V2 (200 + 4) (mineralTableHeight + 8)
+
+    action <- case gs ^. #colonies . at uid of
+      Just colony -> do
+        stockpileTableHeight <- stockpileTable q colony
+        mineTableHeight <- mineTable (q + V2 0 (stockpileTableHeight + 8)) colony
+
+        let r = q + V2 0 (stockpileTableHeight + 8 + mineTableHeight + 8)
+        buildMine <- buildingPanel r colony
+        buildShip <- shipBuildingPanel (r + V2 0 48) colony
+        pure (buildMine <|> buildShip)
+      Nothing ->
+        Widget.button (Rect q (V2 128 20)) "Found colony"
+          <&> whenAlt FoundColony
+
+    pure $ case action of
+      Just BuildMine -> Logic.startBuildingTask uid 0 gs
+      Just BuildShip -> Logic.startShipBuildingTask uid gs
+      Just FoundColony -> Logic.foundColony uid gs
+      Nothing -> gs
+
+  pure (gs' & fromMaybe gs)
+
+bodyList :: V2 Int -> GameState -> Updating (Maybe Body)
+bodyList p gs = do
   (selectedBody, clickedBody) <- use (#ui . #selectedBodyUid) >>= Widget.listBox
     (Rect p (V2 200 500)) 20
     (view #uid) (view #name)
     (gs ^.. #bodies . folded)
   for_ clickedBody $ \Body{ Body.uid } ->
     #ui . #selectedBodyUid .= Just uid
+  pure selectedBody
 
-  gsMay' <- for selectedBody $ \Body{ Body.uid } -> do
-    let minerals = gs ^@.. #bodyMinerals . at uid . _Just . ifolded
-        (mineralLabels, availableLabels, accessibilityLabels) = unzip3 $
-          minerals <&> \(mineral, MineralData{ available, accessibility }) ->
-            ( fromString $ printf "Mineral #%d" mineral
-            , fromString $ printf "%.2f t" available
-            , fromString $ printf "%.0f%%" (100 * accessibility)
-            ) -- TODO table widget?
-    Widget.label (p + V2 (200 + 4) 0) "Mineable resources"
-    Widget.labels (p + V2 (200 + 4) 20) 20 mineralLabels
-    Widget.labels (p + V2 (200 + 4 + 100) 20) 20 availableLabels
-    Widget.labels (p + V2 (200 + 4 + 180) 20) 20 accessibilityLabels
+mineralTable :: V2 Int -> Uid Body -> GameState -> Updating Int
+mineralTable p bodyUid gs =
+  let minerals = gs ^@.. #bodyMinerals . at bodyUid . _Just . ifolded
+      (mineralLabels, availableLabels, accessibilityLabels) = unzip3 $
+        minerals <&> \(mineral, MineralData{ available, accessibility }) ->
+          ( fromString $ printf "Mineral #%d" mineral
+          , fromString $ printf "%.2f t" available
+          , fromString $ printf "%.0f%%" (100 * accessibility)
+          ) -- TODO table widget?
+  in do
+    Widget.label p "Mineable resources"
+    Widget.labels (p + V2 0 20) 20 mineralLabels
+    Widget.labels (p + V2 100 20) 20 availableLabels
+    Widget.labels (p + V2 180 20) 20 accessibilityLabels
+    pure $ (1 + length minerals) * 20
 
-    let q = p + V2 (200 + 4) (length minerals * 20 + 28)
-    case gs ^. #colonies . at uid of
-      Just Colony{ stockpile, mines, buildingTask, shipBuildingTask } -> do
-        Widget.label q "Resource stockpile"
-        let (itemLabels, qtyLabels) = unzip $ itoList stockpile <&> \(mineral, qty) ->
-              ( fromString $ printf "Mineral #%d" mineral
-              , fromString $ printf "%.2f t" qty
-              )
-        Widget.labels (q + V2 0 20) 20 itemLabels
-        Widget.labels (q + V2 100 20) 20 qtyLabels
+stockpileTable :: V2 Int -> Colony -> Updating Int
+stockpileTable p Colony{ stockpile } =
+  let (itemLabels, qtyLabels) = unzip $ itoList stockpile <&> \(mineral, qty) ->
+        ( fromString $ printf "Mineral #%d" mineral
+        , fromString $ printf "%.2f t" qty
+        )
+  in do
+    Widget.label p "Resource stockpile"
+    Widget.labels (p + V2 0 20) 20 itemLabels
+    Widget.labels (p + V2 100 20) 20 qtyLabels
+    pure $ (1 + length stockpile) * 20
 
-        let (mineLabels, mineQtyLabels) = unzip $ itoList mines <&> \(mineral, mineQty) ->
-              ( fromString $ printf "Mineral #%d" mineral
-              , fromString $ printf "%d mines" mineQty
-              )
-            r = q + V2 0 (length stockpile * 20 + 28)
-        Widget.label r "Mining"
-        Widget.labels (r + V2 0 20) 20 mineLabels
-        Widget.labels (r + V2 100 20) 20 mineQtyLabels
+mineTable :: V2 Int -> Colony -> Updating Int
+mineTable p Colony{ mines } =
+  let (mineLabels, mineQtyLabels) = unzip $ itoList mines <&> \(mineral, mineQty) ->
+        ( fromString $ printf "Mineral #%d" mineral
+        , fromString $ printf "%d mines" mineQty
+        )
+  in do
+    Widget.label p "Mining"
+    Widget.labels (p + V2 0 20) 20 mineLabels
+    Widget.labels (p + V2 100 20) 20 mineQtyLabels
+    pure $ (1 + length mines) * 20
 
-        let s = r + V2 0 (length mines * 20 + 8)
-        case buildingTask of
-          Just BuildingTask{ minedMineral } ->
-            Widget.label (s + V2 0 20) (fromString $ printf "Building: Mine for Mineral #%d" minedMineral)
-          Nothing ->
-            Widget.label (s + V2 0 20) "Building: nothing"
-        buildNewMine <- Widget.button (Rect (s + V2 0 40) (V2 256 20)) "Build mine for Mineral #0"
+buildingPanel :: V2 Int -> Colony -> Updating (Maybe Action)
+buildingPanel p Colony{ buildingTask } = do
+  case buildingTask of
+    Just BuildingTask{ minedMineral } ->
+      Widget.label p (fromString $ printf "Building: Mine for Mineral #%d" minedMineral)
+    Nothing ->
+      Widget.label p "Building: nothing"
+  Widget.button (Rect (p + V2 0 20) (V2 256 20)) "Build mine for Mineral #0"
+    <&> whenAlt BuildMine
 
-        case shipBuildingTask of
-          Just ShipBuildingTask{} -> Widget.label (s + V2 0 68) "Producing: Ship"
-          Nothing -> Widget.label (s + V2 0 68) "Producing: nothing"
-        buildNewShip <- Widget.button (Rect (s + V2 0 88) (V2 256 20)) "Produce ship"
-
-        pure $ if
-          | buildNewMine -> Logic.startBuildingTask uid 0 gs
-          | buildNewShip -> Logic.startShipBuildingTask uid gs
-          | otherwise -> gs
-      Nothing -> do
-        found <- Widget.button (Rect q (V2 128 20)) "Found colony"
-        pure $ if found
-          then Logic.foundColony uid gs
-          else gs
-  pure $ fromMaybe gs gsMay'
+shipBuildingPanel :: V2 Int -> Colony -> Updating (Maybe Action)
+shipBuildingPanel p Colony{ shipBuildingTask } = do
+  case shipBuildingTask of
+    Just ShipBuildingTask{} -> Widget.label p "Producing: Ship"
+    Nothing -> Widget.label p "Producing: nothing"
+  Widget.button (Rect (p + V2 0 20) (V2 256 20)) "Produce ship"
+    <&> whenAlt BuildShip
