f4828be8e7796474f4d6e7ff4a4317e170b0a6dc
Viktor Tanyi <tanyi.viktor@gmail.com>
2018-09-09 18:50:21 +0200

    Add more moons

--- src/App/Model/GameState.hs
+++ src/App/Model/GameState.hs
@@ -71,8 +71,23 @@ theRootBody = Body
         , Body{ uid = Uid 11, name = "Deimos", orbitRadius = 0.00015, angularVelocity =  2 * pi / (1.263 * 24 * 60 * 60), children = [] }
         ]
       }
-    , Body{ uid = Uid 4, name = "Jupiter", orbitRadius = 5.20, angularVelocity =  2 * pi / (11.86 * 365 * 24 * 60 * 60), children = [] }
-    , Body{ uid = Uid 5, name = "Saturn", orbitRadius = 9.53, angularVelocity =  2 * pi / (29.44 * 365 * 24 * 60 * 60), children = [] }
+    , Body{ uid = Uid 4, name = "Jupiter", orbitRadius = 5.20, angularVelocity =  2 * pi / (11.86 * 365 * 24 * 60 * 60), children =
+        [ Body{ uid = Uid 12, name = "Io", orbitRadius = 0.0028, angularVelocity =  2 * pi / (1.77 * 24 * 60 * 60), children = [] }
+        , Body{ uid = Uid 13, name = "Europa", orbitRadius = 0.0044, angularVelocity =  2 * pi / (3.55 * 24 * 60 * 60), children = [] }
+        , Body{ uid = Uid 14, name = "Ganymede", orbitRadius = 0.0071, angularVelocity =  2 * pi / (7.15 * 24 * 60 * 60), children = [] }
+        , Body{ uid = Uid 15, name = "Callisto", orbitRadius = 0.0125, angularVelocity =  2 * pi / (16.69 * 24 * 60 * 60), children = [] }
+        ]
+      }
+    , Body{ uid = Uid 5, name = "Saturn", orbitRadius = 9.53, angularVelocity =  2 * pi / (29.44 * 365 * 24 * 60 * 60), children =
+        [ Body{ uid = Uid 16, name = "Mimas", orbitRadius = 0.0012, angularVelocity =  2 * pi / (0.9 * 24 * 60 * 60), children = [] }
+        , Body{ uid = Uid 17, name = "Enceladus", orbitRadius = 0.0016, angularVelocity =  2 * pi / (1.4 * 24 * 60 * 60), children = [] }
+        , Body{ uid = Uid 18, name = "Tethys", orbitRadius = 0.0020, angularVelocity =  2 * pi / (1.9 * 24 * 60 * 60), children = [] }
+        , Body{ uid = Uid 19, name = "Dione", orbitRadius = 0.0025, angularVelocity =  2 * pi / (2.7 * 24 * 60 * 60), children = [] }
+        , Body{ uid = Uid 20, name = "Rhea", orbitRadius = 0.0040, angularVelocity =  2 * pi / (4.5 * 24 * 60 * 60), children = [] }
+        , Body{ uid = Uid 21, name = "Titan", orbitRadius = 0.0081, angularVelocity =  2 * pi / (16 * 24 * 60 * 60), children = [] }
+        , Body{ uid = Uid 22, name = "Iapetus", orbitRadius = 0.0238, angularVelocity =  2 * pi / (79 * 24 * 60 * 60), children = [] }
+        ]
+      }
     , Body{ uid = Uid 6, name = "Uranus", orbitRadius = 19.19, angularVelocity =  2 * pi / (84.01 * 365 * 24 * 60 * 60), children = [] }
     , Body{ uid = Uid 7, name = "Neptune", orbitRadius = 30.06, angularVelocity =  2 * pi / (164.79 * 365 * 24 * 60 * 60), children = [] }
     ]
