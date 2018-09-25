e90ad5385c6e30db611df49b4729a11ffe5a9c16
Viktor Tanyi <tanyi.viktor@gmail.com>
2018-09-25 20:44:33 +0200

    Use UI layout on ship window

--- src/App/Update/ShipWindow.hs
+++ src/App/Update/ShipWindow.hs
@@ -6,13 +6,13 @@ import App.Prelude
 
 import qualified App.Model.Ship as Ship
 import qualified App.Update.Logic as Logic
+import qualified App.Update.Updating as Updating
 import qualified App.Update.Widget as Widget
 import qualified Linear as Lin
 
 import App.Model.Body (Body)
 import App.Model.GameState (GameState(..))
 import App.Model.Ship (Ship(..))
-import App.Rect (Rect(..))
 import App.Uid (Uid)
 import App.Update.Updating (Updating)
 import App.Util (showDate, showDuration, whenAlt)
@@ -24,53 +24,64 @@ data Action
   | CancelOrder
 
 update :: GameState -> Updating GameState
-update gs = do
-  Widget.window (Rect (V2 32 32) (V2 (4 + 600 + 5) (500 + 24))) 20 "Ships"
-  let p = V2 36 56
+update gs =
+  Updating.childLayout "shipWindow" $ do
+    Updating.childBounds "decoration" $ \bounds ->
+      Widget.window bounds 20 "Ships"
 
-  (selectedShip, clickedShip) <- Widget.listBox
-    (Rect p (V2 200 500)) 20
-    (view #uid) (view #name)
-    #selectedShip
-    (gs ^.. #ships . folded)
-  for_ clickedShip $ \ship -> #ui . #editedShipName .= (ship ^. #name)
+    (selectedShip, clickedShip) <-
+      Updating.childBounds "selectedShip" $ \bounds ->
+        Widget.listBox
+          bounds 20
+          (view #uid) (view #name)
+          #selectedShip
+          (gs ^.. #ships . folded)
+    for_ clickedShip $ \ship -> #ui . #editedShipName .= (ship ^. #name)
 
-  gs' <- for selectedShip $ \ship@Ship{ uid } -> do
-    rename <- renamePanel (p + V2 (200 + 4) 0)
-    infoLabels (p + V2 (200 + 4) 28) ship gs
+    gs' <- Updating.childLayout "rightPanel" $
+      for selectedShip $ \ship@Ship{ uid } -> do
+        rename <- Updating.childLayout "renamePanel" renamePanel
+        Updating.childBounds "infoLabels" $ \bounds ->
+          infoLabels (bounds ^. #xy) ship gs
 
-    moveTo <- Widget.button (Rect (p + V2 (200 + 4) 92) (V2 80 20)) "Move to..."
-      <&> whenAlt ()
-    cancel <- Widget.button (Rect (p + V2 (200 + 4 + 80 + 4) 92) (V2 80 20)) "Cancel"
-      <&> whenAlt CancelOrder
+        moveTo <- Updating.childBounds "moveTo" $ \bounds ->
+          Widget.button bounds "Move to..."
+            <&> whenAlt ()
+        cancel <- Updating.childBounds "cancel" $ \bounds ->
+          Widget.button bounds "Cancel"
+            <&> whenAlt CancelOrder
 
-    selectedBody <- Widget.closedDropdown
-      (Rect (p + V2 (200 + 4) 120) (V2 200 20)) 20 380
-      (view #uid) (view #name)
-      #selectedBody
-      (gs ^.. #bodies . folded)
+        selectedBody <- Updating.childBounds "selectedBody" $ \bounds ->
+          Widget.closedDropdown
+            bounds 20 380
+            (view #uid) (view #name)
+            #selectedBody
+            (gs ^.. #bodies . folded)
 
-    _ <- Widget.closedDropdown
-      (Rect (p + V2 (200 + 4) 144) (V2 200 20)) 20 380
-      id (show >>> fromString)
-      #selectedResource
-      [minBound .. maxBound]
+        _ <- Updating.childBounds "selectedResource" $ \bounds ->
+          Widget.closedDropdown
+            bounds 20 380
+            id (show >>> fromString)
+            #selectedResource
+            [minBound .. maxBound]
 
-    let moveToSelected = (moveTo *> selectedBody) <&> \b -> MoveToBody (b ^. #uid)
-        action = rename <|> moveToSelected <|> cancel
-    pure $ case action of
-      Just (Rename name) -> gs & #ships . at uid . _Just . #name .~ name
-      Just (MoveToBody bodyUid) -> Logic.moveShipToBody ship bodyUid gs
-      Just CancelOrder -> Logic.cancelShipOrder ship gs
-      Nothing -> gs
+        let moveToSelected = (moveTo *> selectedBody) <&> \b -> MoveToBody (b ^. #uid)
+            action = rename <|> moveToSelected <|> cancel
+        pure $ case action of
+          Just (Rename name) -> gs & #ships . at uid . _Just . #name .~ name
+          Just (MoveToBody bodyUid) -> Logic.moveShipToBody ship bodyUid gs
+          Just CancelOrder -> Logic.cancelShipOrder ship gs
+          Nothing -> gs
 
-  pure (gs' & fromMaybe gs)
+    pure (gs' & fromMaybe gs)
 
-renamePanel :: V2 Int -> Updating (Maybe Action)
-renamePanel p = do
-  ename <- Widget.textBox "shipName" (Rect p (V2 160 20)) #editedShipName
-  Widget.button (Rect (p + V2 (160 + 4) 0) (V2 100 20)) "Rename"
-    <&> whenAlt (Rename ename)
+renamePanel :: Updating (Maybe Action)
+renamePanel = do
+  ename <- Updating.childBounds "shipName" $ \bounds ->
+    Widget.textBox "shipName" bounds #editedShipName
+  Updating.childBounds "button" $ \bounds ->
+    Widget.button bounds "Rename"
+      <&> whenAlt (Rename ename)
 
 infoLabels :: V2 Int -> Ship -> GameState -> Updating ()
 infoLabels p Ship{ speed, order } gs =
