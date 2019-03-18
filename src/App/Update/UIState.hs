f23348f56c73904b6847b845fabc29c3eee27a97
Viktor Tanyi <tanyi.viktor@gmail.com>
2019-03-18 22:43:07 +0100

    Remove unused UIState fields

--- src/App/Update/UIState.hs
+++ src/App/Update/UIState.hs
@@ -3,15 +3,12 @@ module App.Update.UIState where
 import App.Prelude
 
 import qualified App.Dimension.Local as Local
-import qualified App.Model.Ship as Ship
 import qualified App.UIBuilder.ListBoxState as ListBoxState
 
 import App.Common.Camera (Camera(..))
 import App.Common.Id (Id)
 import App.Dimension.Local (Local)
 import App.Model.Body (Body)
-import App.Model.Installation (Installation)
-import App.Model.Resource (Resource)
 import App.Model.Ship (Ship)
 import App.UIBuilder.ListBoxState (ListBoxState)
 
@@ -22,16 +19,6 @@ data UIState = UIState
   , selectedShip :: ListBoxState (Id Ship)
   -- TODO the body lists on the colony and ship screen are both tied to this
   , selectedBody :: ListBoxState (Id Body)
-  , selectedResource :: ListBoxState Resource
-  , selectedInstallation :: ListBoxState Installation
-  , selectedBuildingTaskIndex :: ListBoxState Int
-  , selectedShipType :: ListBoxState Ship.Type
-  , selectedShipSize :: Int
-  , editedShipName :: Text
-  , editedResourceQty :: Text
-  , editedInstallationQty :: Text
-  , editedPopulationQty :: Text
-  , testNumber :: Int
   }
   deriving (Generic)
 
@@ -50,14 +37,4 @@ initial = UIState
   , activeWindow = Nothing
   , selectedShip = ListBoxState.initial
   , selectedBody = ListBoxState.initial
-  , selectedResource = ListBoxState.initial
-  , selectedInstallation = ListBoxState.initial
-  , selectedBuildingTaskIndex = ListBoxState.initial
-  , selectedShipType = ListBoxState.initial
-  , selectedShipSize = 1
-  , editedShipName = ""
-  , editedResourceQty = ""
-  , editedInstallationQty = ""
-  , editedPopulationQty = ""
-  , testNumber = 0
   }
